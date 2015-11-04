ffdbca0952b5714c8aef239e707600d2faa1b8b3
Jake Plunkett <jacob.d.plunkett@gmail.com>
2015-11-04 02:50:21 +0000

    Fixed Home Routes File

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   devise_for :users
   resources :contacts
   get '/about' => 'pages#about'
-  root 'pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
